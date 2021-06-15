b3d0b823525cae7c331a1324313f1e1249d1596d
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-06-15 01:45:37 -0400

    Update @typescript-eslint to v4.27.0

--- src/rules/typescript-eslint/supported-rules.ts
+++ src/rules/typescript-eslint/supported-rules.ts
@@ -141,7 +141,9 @@ export = {
         '@typescript-eslint/prefer-for-of': 'error',
         '@typescript-eslint/prefer-function-type': 'warn',
         '@typescript-eslint/prefer-includes': 'warn',
-        '@typescript-eslint/prefer-literal-enum-member': 'error',
+        '@typescript-eslint/prefer-literal-enum-member': ['error', {
+            allowBitwiseExpressions: true,
+        }],
         '@typescript-eslint/prefer-namespace-keyword': 'warn',
         '@typescript-eslint/prefer-nullish-coalescing': 'error',
         '@typescript-eslint/prefer-optional-chain': 'error',
