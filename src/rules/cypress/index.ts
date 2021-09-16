237ead1e61e5fd7550960dfc9f09e3f757518a2b
Andrey Mikheychik <a.mikheychik@gmail.com>
2021-09-15 21:46:36 -0500

    Update eslint-plugin-cypress to v2.12.1

--- src/rules/cypress/index.ts
+++ src/rules/cypress/index.ts
@@ -7,6 +7,7 @@ export = {
         'cypress/no-assigning-return-values': 'error',
         'cypress/no-async-tests': 'error',
         'cypress/no-force': 'error',
+        'cypress/no-pause': 'error',
         'cypress/no-unnecessary-waiting': 'error',
         'cypress/require-data-selectors': 'error',
     },
