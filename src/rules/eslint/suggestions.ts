5daa71f30371ca0e45728a4b001a8de5f1417314
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-08-13 14:36:21 -0500

    Remove “Fixable” column from ESLint docs

--- src/rules/eslint/suggestions.ts
+++ src/rules/eslint/suggestions.ts
@@ -141,7 +141,6 @@ export = {
         // Rather disallow using loops at all and use filter/map/reduce instead.
         'no-restricted-syntax': ['error', 'ForInStatement'],
         'no-return-assign': 'error',
-        'no-return-await': 'off',
         'no-script-url': 'error',
         'no-sequences': ['error', {
             allowInParentheses: false,
