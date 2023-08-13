c6e7ceb65f8f86f41324243200c7a82cbcd43cb7
Andrey Mikheychik <a.mikheychik@gmail.com>
2023-08-12 19:46:30 -0500

    Upgrade eslint-plugin-testing-library to v6.0.0
    * Replace `testing-library/await-async-query` with `testing-library/await-async-queries`.
    * Replace `testing-library/await-fire-event` with `testing-library/await-async-events`.
    * Replace `testing-library/no-await-sync-query` with `testing-library/no-await-sync-queries`.
    * Replace `testing-library/no-render-in-setup` with `testing-library/no-render-in-lifecycle`.
    * Remove `testing-library/no-wait-for-empty-callback`.
    * Remove `testing-library/prefer-wait-for`.

--- src/rules/testing-library/index.ts
+++ src/rules/testing-library/index.ts
@@ -3,16 +3,16 @@ export = {
         'testing-library',
     ],
     rules: {
-        'testing-library/await-async-query': 'error',
+        'testing-library/await-async-events': 'off',
+        'testing-library/await-async-queries': 'error',
         'testing-library/await-async-utils': 'error',
         // Vue.js only
-        'testing-library/await-fire-event': 'off',
         'testing-library/consistent-data-testid': ['error', {
             testIdPattern: '^[a-z0-9]+(-[a-z0-9]+)*$',
             testIdAttribute: 'data-testId',
         }],
         'testing-library/no-await-sync-events': 'error',
-        'testing-library/no-await-sync-query': 'error',
+        'testing-library/no-await-sync-queries': 'error',
         'testing-library/no-container': 'error',
         'testing-library/no-debugging-utils': 'error',
         'testing-library/no-dom-import': 'warn',
@@ -20,10 +20,9 @@ export = {
         'testing-library/no-manual-cleanup': 'error',
         'testing-library/no-node-access': 'error',
         'testing-library/no-promise-in-fire-event': 'error',
-        'testing-library/no-render-in-setup': 'error',
+        'testing-library/no-render-in-lifecycle': 'error',
         // React only
         'testing-library/no-unnecessary-act': 'off',
-        'testing-library/no-wait-for-empty-callback': 'error',
         'testing-library/no-wait-for-multiple-assertions': 'error',
         'testing-library/no-wait-for-side-effects': 'error',
         'testing-library/no-wait-for-snapshot': 'error',
@@ -36,9 +35,8 @@ export = {
         'testing-library/prefer-query-by-disappearance': 'error',
         // TODO: Configure matchers
         'testing-library/prefer-query-matchers': 'off',
-        'testing-library/prefer-user-event': 'error',
         'testing-library/prefer-screen-queries': 'error',
-        'testing-library/prefer-wait-for': 'warn',
+        'testing-library/prefer-user-event': 'error',
         'testing-library/render-result-naming-convention': 'error',
     },
 };
