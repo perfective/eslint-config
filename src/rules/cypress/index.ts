d2848b5fdf28b17db281ec5d39aab841e088733b
Andrey Mikheychik <a.mikheychik@gmail.com>
2024-06-08 15:37:03 -0500

    Upgrade eslint-plugin-cypress to v3.3.0
    - Enable the `cypress/no-async-before` rule as an error.

--- src/rules/cypress/index.ts
+++ src/rules/cypress/index.ts
@@ -3,6 +3,7 @@ import { optionalConfig } from '../../config/plugin';
 export = optionalConfig('cypress', {
     'cypress/assertion-before-screenshot': 'error',
     'cypress/no-assigning-return-values': 'error',
+    'cypress/no-async-before': 'error',
     'cypress/no-async-tests': 'error',
     'cypress/no-force': 'error',
     'cypress/no-pause': 'error',
