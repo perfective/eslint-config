39d37d94a68e47e190ba899948fb7aca5839b3b7
Andrey Mikheychik <a.mikheychik@gmail.com>
2024-11-11 20:43:23 -0600

    Disable  @stylistic/plus/curly-newline for empty blocks

--- src/rules/stylistic/plus/index.ts
+++ src/rules/stylistic/plus/index.ts
@@ -3,7 +3,9 @@ export = {
         '@stylistic/plus',
     ],
     rules: {
-        '@stylistic/plus/curly-newline': ['warn', 'always'],
+        '@stylistic/plus/curly-newline': ['warn', {
+            minElements: 1,
+        }],
         '@stylistic/plus/indent-binary-ops': ['warn', 4],
         '@stylistic/plus/type-generic-spacing': 'warn',
         '@stylistic/plus/type-named-tuple-spacing': 'warn',
