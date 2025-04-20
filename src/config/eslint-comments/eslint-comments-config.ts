dfd74e3b535e3be391b4d54054c68d7bcefbec2a
Andrey Mikheychik <a.mikheychik@gmail.com>
2025-04-20 18:03:54 -0500

    Add @eslint-community/eslint-plugin-eslint-comments v4.5.0
    Replace unmaintained `eslint-plugin-eslint-comments`

--- src/config/eslint-comments/eslint-comments-config.ts
+++ src/config/eslint-comments/eslint-comments-config.ts
@@ -1,5 +1,6 @@
 import { Linter } from 'eslint';
-import eslintPluginEslintComments from 'eslint-plugin-eslint-comments';
+
+import eslintPluginEslintComments from '@eslint-community/eslint-plugin-eslint-comments';
 
 import { bestPracticesRules } from './best-practices-rules';
 import { stylisticIssuesRules } from './stylistic-issues-rules';
--- src/config/eslint-comments/eslint-plugin-eslint-comments.d.ts
+++ src/config/eslint-comments/eslint-plugin-eslint-comments.d.ts
@@ -1,4 +1,4 @@
-declare module 'eslint-plugin-eslint-comments' {
+declare module '@eslint-community/eslint-plugin-eslint-comments' {
     const plugin: import('eslint').ESLint.Plugin;
     export default plugin;
 }
