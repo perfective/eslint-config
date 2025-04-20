472d0ce257429fb38852026156e5d23302bdcb84
Andrey Mikheychik <a.mikheychik@gmail.com>
2025-04-20 09:47:03 -0500

    Remove CJS require paths from package.json
    Only ESM code is compiled

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ const full = gulp.series(
     perfectiveGulp.typescript.esmBuild(),
     perfectiveGulp.typescript.tsDeclarations(),
     perfectiveGulp.packageJson.packageJson({
-        main: './index.js',
         module: './index.js',
         types: './index.d.ts',
         directories: {
@@ -23,44 +22,36 @@ const full = gulp.series(
         exports: {
             '.': {
                 import: './index.js',
-                require: './index.js',
                 types: './index.d.ts',
             },
             './config': {
                 import: './config.js',
-                require: './config.js',
                 types: './config.d.ts',
             },
             './rules': {
                 import: './rules.js',
-                require: './rules.js',
                 types: './rules.d.ts',
             },
             // Optional plugins.
             // Each plugin must be in a separate export, so it does not fail when the plugin is not installed.
             './cypress': {
                 import: './cypress.js',
-                require: './cypress.js',
                 types: './cypress.d.ts',
             },
             './jest': {
                 import: './jest.js',
-                require: './jest.js',
                 types: './jest.d.ts',
             },
             './jest-dom': {
                 import: './jest-dom.js',
-                require: './jest-dom.js',
                 types: './jest-dom.d.ts',
             },
             './rxjs': {
                 import: './rxjs.js',
-                require: './rxjs.js',
                 types: './rxjs.d.ts',
             },
             './testing-library': {
                 import: './testing-library.js',
-                require: './testing-library.js',
                 types: './testing-library.d.ts',
             },
         },
