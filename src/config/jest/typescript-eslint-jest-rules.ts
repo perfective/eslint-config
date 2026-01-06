4e60c6104053e4653b2845a0c13f41959fb1d117
Andrey Mikheychik <a.mikheychik@gmail.com>
2026-01-06 12:58:33 -0600

    Update eslint-plugin-jest v29.12.1
    - Enable the `jest/no-error-equal` rule as an error.
    - Enable the `jest/no-unnecessary-assertion` rule as an error.
    - Enable the `jest/no-unneeded-async-expect-function` rule as a warning.
    - Enable the `jest/prefer-mock-return-shorthand` rule as a warning.
    - Enable the `jest/prefer-to-have-been-called` rule as a warning.
    - Enable the `jest/prefer-to-have-been-called-times` rule as a warning.
    - Enable the `jest/valid-expect-with-promise` rule as an error.
    - Enable the `jest/valid-mock-module-path` rule as an error.

--- src/config/jest/typescript-eslint-jest-rules.ts
+++ src/config/jest/typescript-eslint-jest-rules.ts
@@ -1,7 +1,10 @@
 import { Linter } from 'eslint';
 
 export const typescriptEslintJestRules: Linter.RulesRecord = {
+    'jest/no-error-equal': 'error',
+    'jest/no-unnecessary-assertion': 'error',
     'jest/unbound-method': ['error', {
         ignoreStatic: false,
     }],
+    'jest/valid-expect-with-promise': 'error',
 };
