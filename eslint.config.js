fdb4dd231ba3fd47b83bd60e80bad6731ba731f1
Andrey Mikheychik <a.mikheychik@gmail.com>
2026-01-08 17:41:53 -0600

    Tigthen `import/no-extraneous-dependencies` project-level config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import { jestDomConfig } from './dist/config/jest-dom/index.js';
 import { rxjsConfig } from './dist/config/rxjs/index.js';
 import { testingLibraryConfig } from './dist/config/testing-library/index.js';
 import { unicornPreventAbbreviations } from './dist/config/unicorn/index.js';
-import { perfectiveEslintConfig } from './dist/index.js';
+import { configurationFiles, jestFiles, perfectiveEslintConfig } from './dist/index.js';
 
 const eslintConfig = perfectiveEslintConfig([
     cypressConfig,
@@ -17,7 +17,8 @@ const eslintConfig = perfectiveEslintConfig([
     {
         rules: {
             'import/no-extraneous-dependencies': ['error', importNoExtraneousDependencies({
-                devDependencies: true,
+                devDependencies: [...jestFiles, ...configurationFiles],
+                peerDependencies: true,
             })],
             'unicorn/prevent-abbreviations': ['warn', unicornPreventAbbreviations({}, {
                 allowList: {
